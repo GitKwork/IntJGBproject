bd59a17d6929baf590afc892146f70462b1ba0bf
«Kwork» <kwork@yandex.ru>
2022-08-31 15:55:54 +0200

    Add code 8ferzi

---
+++ src/MainHW8ferzi.java
@@ -0,0 +1,103 @@
+public class MainHW8ferzi {
+    public static void main(String[] args) {
+        arrangeFerz();
+    }
+    public static void printMatrix(char[][] board) {
+        // 8x8 board
+        int n = board.length;
+        System.out.println("------------------------------");
+        for (int i = 0; i < n; i++) {
+            System.out.print("|");
+            for (int j = 0; j < n; j++) {
+                System.out.print(board[i][j] + "|");
+            }
+            System.out.println();
+        }
+        System.out.println("------------------------------");
+    }
+    public static void updateBoard(int row, int col, boolean[][] logicalBoard) {
+        int n = logicalBoard.length;
+        for (int j = 0; j < n; j++) {
+            logicalBoard[row][j] = false;
+        }
+        for (int j = 0; j < n; j++) {
+            logicalBoard[j][col] = false;
+        }
+        int r = row;
+        int c = col;
+        while (r >= 0 && c >= 0) {
+            logicalBoard[r][c] = false;
+            r--;
+            c--;
+        }
+        r = row;
+        c = col;
+        while (r < 8 && c < 8) {
+            logicalBoard[r][c] = false;
+            r++;
+            c++;
+        }
+        r = row;
+        c = col;
+        while (r < 8 && c >= 0) {
+            logicalBoard[r][c] = false;
+            r++;
+            c--;
+        }
+        r = row;
+        c = col;
+        while (r >= 0 && c < 8) {
+            logicalBoard[r][c] = false;
+            r--;
+            c++;
+        }
+    }
+
+    public static char[][] copyArray(char[][] original) {
+        char[][] copy = new char[original.length][];
+        for (int i = 0; i < original.length; i++) {
+            copy[i] = original[i].clone();
+        }
+        return copy;
+    }
+
+    public static boolean[][] copyArray(boolean[][] original) {
+        boolean[][] copy = new boolean[original.length][];
+        for (int i = 0; i < original.length; i++) {
+            copy[i] = original[i].clone();
+        }
+        return copy;
+    }
+
+    public static void arrangeFerz(char[][] board, boolean[][] logicalBoard, int rowNumber) {
+        if (rowNumber == 8) {
+            printMatrix(board);
+            return;
+        }
+        int n = board.length;
+        for (int column = 0; column < n; column++) {
+            if (logicalBoard[rowNumber][column]) {
+                char[][] newBoard = copyArray(board);
+                boolean[][] newLogicalBoard = copyArray(logicalBoard);
+                newBoard[rowNumber][column] = 'F';
+                newLogicalBoard[rowNumber][column] = false;
+                updateBoard(rowNumber, column, newLogicalBoard);
+                arrangeFerz(newBoard, newLogicalBoard, rowNumber + 1);
+            }
+        }
+    }
+
+    public static void arrangeFerz() {
+        int N = 8;
+        char[][] board = new char[8][8];
+        boolean[][] logicalBoard = new boolean[8][8];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                board[i][j] = ' ';
+                logicalBoard[i][j] = true;
+            }
+        }
+        arrangeFerz(board, logicalBoard, 0);
+    }
+
+}
